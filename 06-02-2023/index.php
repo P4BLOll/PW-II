4cb5eebb9e48f324c3da0d6606ecd7e2c893a91c
Pablo Santos <99838818+P4BLOll@users.noreply.github.com>
2023-02-09 13:12:22 -0300

    Aula de PHP

---
+++ 06-02-2023/index.php
@@ -0,0 +1,12 @@
+<?php 
+
+$a = 1;
+$b = 2;
+
+echo "Adição: " . $a + $b . "<br>";
+echo "Subtraçaõ: " . $a - $b . "<br>";
+echo "Multplicação " . $a * $b . "<br>";
+echo "Divisão: " . $a / $b . "<br>";
+
+
+?>
